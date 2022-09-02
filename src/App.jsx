20abc9da93a6344121a4d27c844089c3e1ce2ac8
Nick Danelia <nikdanelia@gmail.com>
2022-09-02 14:22:54 +0400

    Fix local storage bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,92 +7,94 @@ import Employees from "./Employees";
 import GroupedTeamMembers from "./GroupedTeamMembers";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      fullName: "ლაშა ნოზაძე",
-      designation: "JavaScript Developer",
-      gender: "male",
-      teamName: "გუნდი 1",
-    },
-    {
-      id: 2,
-      fullName: "ნინო აბულაძე",
-      designation: "Node Developer",
-      gender: "female",
-      teamName: "გუნდი 1",
-    },
-    {
-      id: 3,
-      fullName: "მარიამ კვარაცხელია",
-      designation: "Java Developer",
-      gender: "female",
-      teamName: "გუნდი 1",
-    },
-    {
-      id: 4,
-      fullName: "ნიკა ხუციშვილი",
-      designation: "React Developer",
-      gender: "male",
-      teamName: "გუნდი 2",
-    },
-    {
-      id: 5,
-      fullName: "გოჩა მიქელაძე",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "გუნდი 2",
-    },
-    {
-      id: 6,
-      fullName: "ანა ბაირამოვი",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "გუნდი 2",
-    },
-    {
-      id: 7,
-      fullName: "მამუკა შენგელია",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "გუნდი 3",
-    },
-    {
-      id: 8,
-      fullName: "თაკო ტაბატაძე",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "გუნდი 3",
-    },
-    {
-      id: 9,
-      fullName: "მაია დიასამიძე",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "გუნდი 3",
-    },
-    {
-      id: 10,
-      fullName: "ზაზა აბაშიძე",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "გუნდი 4",
-    },
-    {
-      id: 11,
-      fullName: "ანდრია ბერიძე",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "გუნდი 4",
-    },
-    {
-      id: 12,
-      fullName: "ილია მამედოვი",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "გუნდი 4",
-    },
-  ]);
+  const [employees, setEmployees] = useState(
+    JSON.parse(localStorage.getItem("employeeList")) || [
+      {
+        id: 1,
+        fullName: "ლაშა ნოზაძე",
+        designation: "JavaScript Developer",
+        gender: "male",
+        teamName: "გუნდი 1",
+      },
+      {
+        id: 2,
+        fullName: "ნინო აბულაძე",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "გუნდი 1",
+      },
+      {
+        id: 3,
+        fullName: "მარიამ კვარაცხელია",
+        designation: "Java Developer",
+        gender: "female",
+        teamName: "გუნდი 1",
+      },
+      {
+        id: 4,
+        fullName: "ნიკა ხუციშვილი",
+        designation: "React Developer",
+        gender: "male",
+        teamName: "გუნდი 2",
+      },
+      {
+        id: 5,
+        fullName: "გოჩა მიქელაძე",
+        designation: "DotNet Developer",
+        gender: "male",
+        teamName: "გუნდი 2",
+      },
+      {
+        id: 6,
+        fullName: "ანა ბაირამოვი",
+        designation: "SQL Server DBA",
+        gender: "female",
+        teamName: "გუნდი 2",
+      },
+      {
+        id: 7,
+        fullName: "მამუკა შენგელია",
+        designation: "Angular Developer",
+        gender: "male",
+        teamName: "გუნდი 3",
+      },
+      {
+        id: 8,
+        fullName: "თაკო ტაბატაძე",
+        designation: "API Developer",
+        gender: "female",
+        teamName: "გუნდი 3",
+      },
+      {
+        id: 9,
+        fullName: "მაია დიასამიძე",
+        designation: "C++ Developer",
+        gender: "female",
+        teamName: "გუნდი 3",
+      },
+      {
+        id: 10,
+        fullName: "ზაზა აბაშიძე",
+        designation: "Python Developer",
+        gender: "male",
+        teamName: "გუნდი 4",
+      },
+      {
+        id: 11,
+        fullName: "ანდრია ბერიძე",
+        designation: "Vue Developer",
+        gender: "male",
+        teamName: "გუნდი 4",
+      },
+      {
+        id: 12,
+        fullName: "ილია მამედოვი",
+        designation: "Graphic Designer",
+        gender: "male",
+        teamName: "გუნდი 4",
+      },
+    ]
+  );
   const [selectedTeam, setSelectedTeam] = useState(
     JSON.parse(localStorage.getItem("selectedTeam")) || "გუნდი 1"
   );
@@ -120,31 +122,29 @@ function App() {
   }
   return (
     <Router>
-      <div className="App">
-        <Header
-          selectedTeam={selectedTeam}
-          teamMemberCount={
-            employees.filter((employee) => employee.teamName === selectedTeam)
-              .length
+      <Header
+        selectedTeam={selectedTeam}
+        teamMemberCount={
+          employees.filter((employee) => employee.teamName === selectedTeam)
+            .length
+        }
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Employees
+              handleEmployeeCardClick={handleEmployeeCardClick}
+              employees={employees}
+              handleTeamSelectionChange={handleTeamSelectionChange}
+              selectedTeam={selectedTeam}
+            />
           }
-        />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Employees
-                handleEmployeeCardClick={handleEmployeeCardClick}
-                employees={employees}
-                handleTeamSelectionChange={handleTeamSelectionChange}
-                selectedTeam={selectedTeam}
-              />
-            }
-          ></Route>
-          <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers />} />
-        </Routes>
+        ></Route>
+        <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers />} />
+      </Routes>
 
-        <Footer />
-      </div>
+      <Footer />
     </Router>
   );
 }
